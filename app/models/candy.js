b2b45e01b646fb9f72aaab8e69cd93cb2fd666d4
harrisimon <harrisimon@gmail.com>
2022-10-25 18:10:36 -0400

    set up schema, created route file, created create-route, imported into server.js

---
+++ app/models/candy.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const candySchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		flavor: {
+			type: String,
+			required: true,
+		},
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+module.exports = mongoose.model('Candy', candySchema)
